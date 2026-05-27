bb5546a3089dba283c63ef50866addd4f056c0e6
SriRam Cherukuri <sriramcherukuri92@gmail.com>
2026-05-27 17:00:46 +0530

    Added production monitoring with Sentry

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -7,6 +7,7 @@ import { BASE_URL } from "../utils/constants";
 import { validate } from "../utils/validation";
 import { motion } from "motion/react";
 
+
 const Login = () => {
   const [emailid, setEmailid] = useState("");
   const [password, setPassword] = useState("");
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from '../src/Components/App'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://5c39ab4346174801814bf41023586b94@o4511461435506688.ingest.de.sentry.io/4511461449138256",
+  sendDefaultPii: true,
+  tracesSampleRate: 1.0,
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
